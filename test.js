64f945678a15adafee33cc3cf5906f635bcbfc17
jonschlinkert <jon.schlinkert@sellside.com>
2015-03-05 13:24:42 -0500

    run update

--- test.js
+++ test.js
@@ -1,8 +1,8 @@
 /*!
- * is-absolute <https:\\github.com/jonschlinkert/is-absolute>
+ * is-absolute <https://github.com/jonschlinkert/is-absolute>
  *
- * Copyright (c) 2014 Jon Schlinkert, contributors.
- * Licensed under the MIT License
+ * Copyright (c) 2014-2015, Jon Schlinkert.
+ * Licensed under the MIT License.
  */
 
 'use strict';
